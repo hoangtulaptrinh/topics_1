056781f296d17d2f398b5938dd930c59cf9bc8f5
DatNT2-F1-D1 <datnt2@fabbi.io>
2020-11-30 17:32:20 +0700

    bug from user page

--- src/admin/Users/index.js
+++ src/admin/Users/index.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { Table, Space, Modal, Input, Button, Form } from 'antd';
 // import { FaBan } from 'react-icons/fa';
 import { DollarOutlined } from '@ant-design/icons';
 import { connect } from 'react-redux';
 import { getAllUsers, updateUser } from '../../actions/index';
+import { useForm } from 'antd/lib/form/Form';
 
 const layout = {
   labelCol: {
@@ -13,6 +14,7 @@ const layout = {
     span: 16,
   },
 };
+
 const tailLayout = {
   wrapperCol: {
     offset: 16,
@@ -20,22 +22,28 @@ const tailLayout = {
   },
 };
 
-class Users extends Component {
-  formRef = React.createRef();
+function Users({ fetchAllUser, updateUser, listUser }) {
+  // const [visible, setVisible] = useState(false);
+  // const [isAddNew, setIsAddNew] = useState(false);
+  const [visibleModalAddMoney, setVisibleModalAddMoney] = useState(false);
+  const [coin, setCoin] = useState('');
+  const [coinAdd, setCoinAdd] = useState(0);
+  const [userId, setUserId] = useState('');
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: false,
-      isAddNew: false,
-      visibleModalAddMoney: false,
-      coin: '',
-      coinAdd: 0,
-      userId: '',
-    };
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     visible: false,
+  //     isAddNew: false,
+  //     visibleModalAddMoney: false,
+  //     coin: '',
+  //     coinAdd: 0,
+  //     userId: '',
+  //   };
+  // }
 
-  columns = [
+  const [form] = Form.useForm();
+  const columns = [
     // {
     //   align: "center",
     //   title: "STT",
@@ -73,108 +81,86 @@ class Users extends Component {
       key: 'action',
       render: (_, record) => (
         <Space size="middle">
-          <DollarOutlined className="styled-icon" onClick={() => this.showModalAddMoney(record)} />
+          <DollarOutlined className="styled-icon" onClick={() => showModalAddMoney(record)} />
         </Space>
       ),
     },
   ];
 
-  data = [
-    {
-      stt: '1',
-      name: 'Kim Anh',
-      password: 'Kim Anh',
-      email: '123ntd@gmail.com',
-      phone_number: '0353689521',
-      money: '250.000',
-      status: 'Đang hoạt động',
-    },
-  ];
+  const onFinish = () => handleAddCoin();
 
-  onFinish = () => this.handleAddCoin();
+  // const showModalDetail = () => {
+  //   this.setState({
+  //     visible: true,
+  //     isAddNew: false,
+  //   });
+  // };
 
-  showModalDetail = () => {
-    this.setState({
-      visible: true,
-      isAddNew: false,
+  const showModalAddMoney = record => {
+    setVisibleModalAddMoney(true);
+    setCoin(record.money);
+    setUserId(record._id);
+  };
+
+  const handleCanCelModalAddMoney = () => {
+    setVisibleModalAddMoney(false);
+  };
+
+  const handleAddCoin = () => {
+    const fieldValue = form.getFieldsValue();
+    console.log(fieldValue, 'co gi thay doi k');
+    let coinValue = Number(fieldValue.current_coin) + Number(fieldValue.coin_add);
+    console.log(coinValue, 'coin value');
+    setCoin(coinValue);
+    console.log('coin sau khi add', coin);
+    setVisibleModalAddMoney(false);
+    updateUser({
+      id: userId,
+      money: coin,
     });
   };
 
-  showModalAddMoney = record => {
-    this.setState({
-      visibleModalAddMoney: true,
-      coin: record.money,
-      userId: record._id,
-    });
-  };
-
-  handleCancel = () => {
-    this.setState({
-      visible: false,
-    });
-  };
-
-  handleCanCelModalAddMoney = () => {
-    this.setState({
-      visibleModalAddMoney: false,
-    });
-  };
-
-  handleAddCoin = () => {
-    const fieldValue = this.formRef.current.getFieldsValue();
-    this.setState({
-      coin: Number(fieldValue.current_coin) + Number(fieldValue.coin_add),
-      visibleModalAddMoney: false,
-    });
-    console.log('coin sau khi add', this.state.coin);
-    this.props.updateUser({
-      id: this.state.userId,
-      money: this.state.coin,
-    });
-  };
-
-  render() {
-    const { visibleModalAddMoney, coin, coinAdd } = this.state;
-
-    return (
-      <div className="admin-management">
-        <div className="feature-add">
-          <h2> Danh sách người dùng </h2>
-        </div>
-        <Table dataSource={this.props.listUser.listUsers} columns={this.columns} bordered />
-        <Modal
-          title="Thêm coin"
-          onOK={this.onOkModalAddMoney}
-          onCancel={this.handleCanCelModalAddMoney}
-          visible={visibleModalAddMoney}
-          footer={null}
-        >
-          <Form ref={this.formRef} {...layout} name="form-add-money" onFinish={this.onFinish}>
-            <Form.Item label="Số coin" name="current_coin" initialValue={coin}>
-              <Input type="number" disabled />
-            </Form.Item>
-            <Form.Item label="Số coin vừa nạp" name="coin_add" initialValue={coinAdd}>
-              <Input type="number" />
-            </Form.Item>
-            <Form.Item {...tailLayout} style={{ marginBottom: '12px' }}>
-              <Button type="primary" style={{ marginRight: 12, width: '64px' }} htmlType="submit">
-                Thêm
-              </Button>
-              <Button type="primary" onClick={this.handleCanCelModalAddMoney} style={{ width: '64px' }} danger>
-                Hủy
-              </Button>
-            </Form.Item>
-          </Form>
-        </Modal>
+  const showModalAddUser = () => {};
+  return (
+    <div className="admin-management">
+      <div className="feature-add">
+        <Button type="primary" onClick={showModalAddUser} style={{ margin: '10px 0px', height: '5%' }}>
+          Thêm mới người dùng
+        </Button>
       </div>
-    );
-  }
+      <Table dataSource={listUser} columns={columns} bordered />
+      <Modal
+        title="Thêm coin"
+        // onOK={onOkModalAddMoney}
+        onCancel={handleCanCelModalAddMoney}
+        visible={visibleModalAddMoney}
+        footer={null}
+      >
+        <Form form={form} {...layout} name="form-add-money" onFinish={onFinish}>
+          <Form.Item label="Số coin" name="current_coin" initialValue={coin}>
+            <Input type="number" disabled />
+          </Form.Item>
+          <Form.Item label="Số coin vừa nạp" name="coin_add" initialValue={coinAdd}>
+            <Input type="number" />
+          </Form.Item>
+          <Form.Item {...tailLayout} style={{ marginBottom: '12px' }}>
+            <Button type="primary" style={{ marginRight: 12, width: '64px' }} htmlType="submit">
+              Thêm
+            </Button>
+            <Button type="primary" onClick={handleCanCelModalAddMoney} style={{ width: '64px' }} danger>
+              Hủy
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </div>
+  );
 }
 
 const mapStateToProps = state => {
   console.log(state);
   return {
-    listUser: state.listUsers,
+    listUser: state.listUsers.listUsers,
   };
 };
 
