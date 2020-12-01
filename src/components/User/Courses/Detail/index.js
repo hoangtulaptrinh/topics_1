4171aeb111e706073d3a062fd8d4f61996b03e12
amelie-98 <duong98kk@gmail.com>
2020-12-01 22:12:25 +0700

    test

--- src/components/User/Courses/Detail/index.js
+++ src/components/User/Courses/Detail/index.js
@@ -80,7 +80,6 @@ const HomePage = ({ buyThisCourse, listCourses }) => {
               <div className="video__left">Danh sách bài học</div>
               <div className="video__right">
                 <span>{course.lesson.length} bài học</span>
-                <span>50:00</span>
               </div>
             </div>
 
@@ -93,7 +92,6 @@ const HomePage = ({ buyThisCourse, listCourses }) => {
                     {!!diffDayUpdate(lesson.date_create) && (
                       <span>Cập nhật {diffDayUpdate(lesson.date_create)} ngày trước</span>
                     )}
-                    <span>50:00</span>
                   </div>
                 </div>
               ))}
@@ -112,10 +110,6 @@ const HomePage = ({ buyThisCourse, listCourses }) => {
                 <div className="info">
                   <BookFilled />
                   <span className="content">Tổng số {course.lesson.length} bài học</span>
-                </div>
-                <div className="info">
-                  <AiFillClockCircle />
-                  <span className="content">Cần 50 giờ để học</span>
                 </div>
                 <div className="info">
                   <FaLaptopCode />
